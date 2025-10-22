71fa7ffd677dcc6032b2422813cf8a0835f765c4
Moritz Baur <baurmo01@thu.de>
2025-10-22 20:18:10 +0200

    [FEAT]  Added GUI-Main-Window and GUI-Libraries (#67)
    * Added UIManager for basic ImGui window setup
    
    * Move submodules glfw, googletest, imgui to Software/Produkt/external/
    
    Signed-off-by: Moritz Baur <baurmo01@thu.de>
    
    * Set version of Submodules
    - googletest to v1.17.0
    - glfw to v3.4
    - imgui to v1.92.4
    
    ---------
    
    Signed-off-by: Moritz Baur <baurmo01@thu.de>
    Co-authored-by: Ludas-63 <szonja.panna.weisz@gmail.com>
    Co-authored-by: 4Source <my.log.in.dennis@gmail.com>

---
+++ Software/Produkt/include/ui/UIManager.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifdef __APPLE__
+#define GL_SILENCE_DEPRECATION
+#endif
+
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <string>
+
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+
+class UIManager {
+public:
+    UIManager(int width, int height, const std::string& title); // create window
+    ~UIManager();
+
+    void run();  // start main loop
+    GLFWwindow* getWindow() { return window; }
+
+private:
+    GLFWwindow* window; // pointer to GLFW window
+};
