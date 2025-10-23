03f0b77b5fa85ceea63c65306f74e35fac3d06b5
Moritz Baur <baurmo01@thu.de>
2025-10-23 16:08:47 +0200

    Fixed Unix Scripts
    Signed-off-by: Moritz Baur <baurmo01@thu.de>

--- Software/Produkt/run_clang_tidy.sh
+++ Software/Produkt/run_clang_tidy.sh
@@ -1,1 +1,1 @@
-find Software/Produkt/{src,include,tests} \( -name '*.cpp' -o -name '*.h' \) -print0 | xargs -0 -n1 clang-tidy --p=Software/Produkt/build || true
+find {src,include,tests} \( -name '*.cpp' -o -name '*.h' \) -print0 | xargs -0 -n1 clang-tidy --p=build || true
