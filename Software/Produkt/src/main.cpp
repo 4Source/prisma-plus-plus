6d4da4e378955757d97deb622aa582c7782dcac9
Moritz Baur <baurmo01@thu.de>
2025-10-29 21:19:25 +0100

    Added NoLint for Windows
    Signed-off-by: Moritz Baur <baurmo01@thu.de>

--- Software/Produkt/src/main.cpp
+++ Software/Produkt/src/main.cpp
@@ -1,32 +1,27 @@
+#include "ui/UIManager.h"
+#include <firstcode.h>
 #include <iostream>
 #include <stdexcept>
-#include <firstcode.h>
-#include "ui/UIManager.h"
 
-int main() {
+int main() { // NOLINT(bugprone-exception-escape)
     try {
         std::cout << "Hello Prisma++" << "\n";
-        std::cout << "Add-Lib: " << add(1,2) << "\n";
-        
+        std::cout << "Add-Lib: " << add(1, 2) << "\n";
+
         //  create window
         UIManager ui(800, 600, "Prisma++");
-        ui.run();                           // run main UI loop
+        ui.run(); // run main UI loop
         return 0;
-    }
-    catch (const std::exception& e) {
+    } catch (const std::exception &e) {
         // Handle it internally to prevent it from escaping.
         // Note: I/O itself can still throw, but this is the intent.
         std::cerr << "ERROR: " << e.what() << "\n";
-    }
-    catch (...) {
+    } catch (...) {
         std::cerr << "ERROR: Unknown exception caught." << "\n";
     }
 
     // Since we caught the exception, we can still exit cleanly with an error code.
-    return 1; 
-}
-
-int test()
-{
     return 1;
 }
+
+int test() { return 1; }
