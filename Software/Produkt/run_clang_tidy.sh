ce09de6dae3ad15edf515196625240ce81b24dee
Peter PC <peter.claass@gmail.com>
2025-11-10 19:06:50 +0100

    Got everything running with clang-tidy checks

--- Software/Produkt/run_clang_tidy.sh
+++ Software/Produkt/run_clang_tidy.sh
@@ -2,5 +2,5 @@
 set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 find "$DIR/src" "$DIR/tests" -type f -name '*.cpp' -print0 | \
-  xargs -0 -I{} clang-tidy "{}" --p="$DIR/build"
+  xargs -0 -P8 -I{} clang-tidy "{}" --p="$DIR/build"
 
--- Software/Produkt/tests/test_firstcode.cpp
+++
@@ -1,6 +0,0 @@
-#include "firstcode.h"
-#include <gtest/gtest.h>
-
-TEST(AdditionTest, HandlesPositiveNumbers) {}
-
-TEST(AdditionTest, HandlesNegativeNumbers) {}
