85e32d9c90c602b01e94b4326164a8b05304b7f2
Peter PC <peter.claass@gmail.com>
2025-11-10 21:18:37 +0100

    Removed wrong build dir

--- build/external/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/peterc/Documents/GitRepos/Prisma++/prisma-plus-plus/Software/Produkt/external/googletest/googlemock
-# Build directory: /home/peterc/Documents/GitRepos/Prisma++/prisma-plus-plus/build/external/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/runTests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/peterc/Documents/GitRepos/Prisma++/prisma-plus-plus/build/runTests[1]_tests.cmake")
-  include("/home/peterc/Documents/GitRepos/Prisma++/prisma-plus-plus/build/runTests[1]_tests.cmake")
-else()
-  add_test(runTests_NOT_BUILT runTests_NOT_BUILT)
-endif()
