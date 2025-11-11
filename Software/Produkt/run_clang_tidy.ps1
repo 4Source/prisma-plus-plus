ad7a6b98449af964f78bf93e79fdcdebaccca900
4Source <my.log.in.dennis@gmail.com>
2025-11-11 10:31:26 +0100

    Allow 8 Threads in parallel

--- Software/Produkt/run_clang_tidy.ps1
+++ Software/Produkt/run_clang_tidy.ps1
@@ -4,11 +4,11 @@ $exit_code = 0
 
 foreach ($f in $folders) {
     $fullPath = Join-Path $basePath $f
-    Get-ChildItem -Path $fullPath -Recurse -Include *.cpp -File | ForEach-Object {
+    Get-ChildItem -Path $fullPath -Recurse -Include *.cpp -File | ForEach-Object -Parallel {
         clang-tidy $_.FullName --p="$basePath/build"
         if ($LASTEXITCODE -ne 0) {
             $exit_code = $LASTEXITCODE
         }
-    }
+    } -ThrottleLimit 8
 }
 exit $exit_code
