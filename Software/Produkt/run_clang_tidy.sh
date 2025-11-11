87f2441d6c3801d5ee158a4190258fdcef8d00c3
Moritz Baur <baurmo01@thu.de>
2025-11-11 21:58:32 +0100

    Run clang tidy on multiple cores
    Signed-off-by: Moritz Baur <baurmo01@thu.de>

--- Software/Produkt/run_clang_tidy.sh
+++ Software/Produkt/run_clang_tidy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+JOBS=$(nproc) # Number of available CPU cores
 find "$DIR/src" "$DIR/tests" -type f -name '*.cpp' -print0 | \
-  xargs -0 -I{} clang-tidy "{}" --p="$DIR/build"
+  xargs -0 -P "$JOBS" -I{} clang-tidy "{}" --p="$DIR/build"
 
