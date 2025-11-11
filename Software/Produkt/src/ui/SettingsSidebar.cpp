717dec9bb3819f12087dae51306322dd6e4896a8
PC-512 <thedia66@googlemail.com>
2025-11-11 20:37:45 +0100

    Update SettingsSidebar.cpp

--- Software/Produkt/src/ui/SettingsSidebar.cpp
+++ Software/Produkt/src/ui/SettingsSidebar.cpp
@@ -40,18 +40,108 @@ void SettingsSidebar::draw(int windowWidth, int windowHeight) {
 
     // --- Objects Section ---
     if (ImGui::CollapsingHeader("Objects", ImGuiTreeNodeFlags_DefaultOpen)) {
-      // TODO: Add object settings
+
+      // Placeholders for visual demonstration
+      static float transparency = 0.5f;
+      static float reflectivity = 0.8f;
+      static float color[3] = {1.0f, 0.5f, 0.0f};
+      static int materialIndex = 0;
+      static float position[3] = {10.0f, 5.0f, 0.0f};
+
+      ImGui::Separator();
+      ImGui::Text("Transparency");
+      if (ImGui::SliderFloat("##transparency", &transparency, 0.00f, 1.00f,
+          "%.2f")) {
+        std::cout << "Transparency: " << transparency << "\n";
+      }
+
+      ImGui::Separator();
+      ImGui::Text("Reflectivity");
+      if (ImGui::SliderFloat("##refelctivity", &reflectivity, 0.00f, 1.00f,
+          "%.2f")) {
+        std::cout << "Reflectivity: " << reflectivity << "\n";
+      }
+
+      ImGui::Separator();
+      ImGui::Text("Material Type");
+      const char *materialItems[] = {"Plastic", "Metal", "Glass", "Ceramic"};
+      const char *currentMaterial = materialItems[materialIndex];
+
+      if (ImGui::BeginCombo("##material", currentMaterial)) {
+        for (int n = 0; n < IM_ARRAYSIZE(materialItems); n++) {
+          bool is_selected = (currentMaterial == materialItems[n]);
+          if (ImGui::Selectable(materialItems[n], is_selected)) {
+            materialIndex = n;
+            std::cout << "Material changed to: " << materialItems[n]
+                      << "\n";
+          }
+          if (is_selected) {
+            ImGui::SetItemDefaultFocus();
+          }
+        }
+        ImGui::EndCombo();
+      }
+
+      ImGui::Separator();
+      if (ImGui::ColorEdit3("Object Color", color,
+          ImGuiColorEditFlags_NoInputs)) {
+      }
+
+      ImGui::Separator();
+      ImGui::Spacing();
+      ImGui::Text("Position (X, Y, Z)");
+      ImGui::Separator();
+
+      float v_speed = 0.01f;
+      float v_min = -FLT_MAX;
+      float v_max = +FLT_MAX;
+
+      if (ImGui::DragFloat3("##postionObject", position, v_speed, v_min, v_max,
+          "%.2f")) {
+        std::cout << "Position: (" << position[0] << ", " << position[1] << ", "
+                  << position[2] << ")\n";
+      }
     }
 
     ImGui::Separator();
 
     // --- Lights Section ---
     if (ImGui::CollapsingHeader("Lights", ImGuiTreeNodeFlags_DefaultOpen)) {
-      // TODO: Add light settings
-    }
 
+      // Placeholders for visual demonstration
+      static float intensity = 0.5f;
+      static float reflectivity = 0.8f;
+      static float color[3] = {1.0f, 0.5f, 0.0f};
+      static int materialIndex = 0;
+      static float position[3] = {10.0f, 5.0f, 0.0f};
+
+      ImGui::Separator();
+      ImGui::Text("Intensity");
+      if (ImGui::SliderFloat("##intensity", &intensity, 0.00f, 1.00f, "%.2f")) {
+        std::cout << "Intensity: " << intensity << "\n";
+      }
+
+      ImGui::Separator();
+      if (ImGui::ColorEdit3("Light Color", color, ImGuiColorEditFlags_NoInputs)) {
+      }
+      
+      ImGui::Separator();
+      ImGui::Spacing();
+      ImGui::Text("Position (X, Y, Z)");
+      ImGui::Separator();
+
+      float v_speed = 0.01f;
+      float v_min = -FLT_MAX;
+      float v_max = +FLT_MAX;
+
+      if (ImGui::DragFloat3("##postionLight", position, v_speed, v_min, v_max,
+          "%.2f")) {
+        std::cout << "Position: (" << position[0] << ", " << position[1] << ", "
+                  << position[2] << ")\n";
+      }
+    } 
+    
     ImGui::EndChild();
-
     ImGui::End();
   }
 
