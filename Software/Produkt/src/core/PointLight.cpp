09aeac50489eea2aa171965c7b82409a2a162125
Jan Wangerin <wangja01@thu.de>
2025-11-11 17:08:27 +0100

    fixed merge conflicts pull request #109
    Signed-off-by: Jan Wangerin <wangja01@thu.de>

--- Software/Produkt/src/core/PointLight.cpp
+++ Software/Produkt/src/core/PointLight.cpp
@@ -4,4 +4,4 @@ PointLight::PointLight() : Light{} {}
 
 PointLight::PointLight(glm::vec3 pos, glm::vec3 color, float intensity) : m_Pos{pos}, Light{color, intensity} {}
 
-Ray PointLight::getRay(const glm::vec3 worldPos) const { return worldPos - this->m_Pos; }
+glm::vec3 PointLight::getRay(const glm::vec3 worldPos) const { return worldPos - this->m_Pos; }
