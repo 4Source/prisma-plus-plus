1158ecac37380376f6ae34e7e740e2683f5c00b6
Peter <claape01@thu.de>
2025-11-20 12:57:24 +0100

    Removed Comments

--- Software/Produkt/include/core/Material.hpp
+++ Software/Produkt/include/core/Material.hpp
@@ -96,28 +96,3 @@ class Material {
         return ss.str();
     }
 };
-
-/*
-#pragma once
-#define GLM_ENABLE_EXPERIMENTAL
-#include <glm/glm.hpp>
-#include <string>
-
-class Material {
- private:
-   glm::vec3 m_Color;
-   // TODO: Add material attributes
- public:
-   Material() : m_Color{glm::vec3{1.0f}} {};
-   Material(glm::vec3 color) : m_Color(color) {};
-
-   glm::vec3 get_color() const { return m_Color; };
-
-   std::string toString(bool formatted = false, int indentLevel = 0) {
-       // TODO: toString Material
-       return "TODO: Material";
-   };
-
-}
-;
-  */
