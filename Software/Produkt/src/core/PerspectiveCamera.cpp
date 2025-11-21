b30c8727684c37f687c6457ebf683efedaef4d06
4Source <my.log.in.dennis@gmail.com>
2025-11-21 17:29:20 +0100

    Added include to camera

--- Software/Produkt/src/core/PerspectiveCamera.cpp
+++ Software/Produkt/src/core/PerspectiveCamera.cpp
@@ -1,4 +1,5 @@
  #include "core/PerspectiveCamera.hpp"
+ #include "core/glm_json.hpp"
 
 void to_json(nlohmann::json &j, const PerspectiveCamera &camera){};
 void from_json(const nlohmann::json &j, PerspectiveCamera &camera){};
