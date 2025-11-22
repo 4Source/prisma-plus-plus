8a8f68f0a6f23716f00984cc0383ae3026b43c64
Jan Wangerin <wangja01@thu.de>
2025-11-22 19:06:41 +0100

    deleted default constructor in PerspectiveCamera
    Signed-off-by: Jan Wangerin <wangja01@thu.de>

--- Software/Produkt/include/core/PerspectiveCamera.hpp
+++ Software/Produkt/include/core/PerspectiveCamera.hpp
@@ -6,7 +6,6 @@
 class PerspectiveCamera : public Camera {
   public:
     PerspectiveCamera(glm::vec3 pos, glm::vec3 up, glm::vec3 look_at, float d, float w, int res_h, int res_v) : Camera{pos, up, look_at, d, w, res_h, res_v} {}
-    PerspectiveCamera() = default;
     friend void to_json(nlohmann::json &j, const PerspectiveCamera &camera);
     friend void from_json(const nlohmann::json &j, PerspectiveCamera &camera);
 };
